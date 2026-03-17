0b9c23fd9154d486bbafb6dba87108ad1d407306
Karan Negi <karanworkws@gmail.com>
2026-03-17 19:13:04 +0530

    intial clean commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-ai-assistant
+This repository contains a practice AI chatbot project developed for learning and experimentation purposes. The main goal of this project is to understand how conversational AI systems work, including user input handling, response generation, and basic chatbot logic. 
---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
+
+  res.json({ reply: "hi !" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
